bfa7b7b2a08c4540f5c70918a706a82ce11819ff
Andre Kovac <me@andrekovac.com>
2016-06-24 01:29:56 +0200

    Delete former api handler

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import submitHandler from './submitHandler';
-import { localUrl, baseUrl } from './config';
-
-export const url = baseUrl;
-
-export default {
-  url: baseUrl,
-  submit: submitHandler,
-};
