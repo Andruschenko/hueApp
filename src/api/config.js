0d2412039657ef4995b35b23ca0d4b0795269d4b
Andre Kovac <me@andrekovac.com>
2016-11-29 12:42:27 +0200

    Add new backend domain

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
 export const localUrl = 'http://192.168.99.100:5000/api/v1'; // local docker
-export const baseUrl = 'http://46.101.213.36:5000/api/v1';  // digital ocean server
+export const baseUrl = 'http://clairecut.tentacula.me/api/v1';  // new server
 export const accessKey = 'myAccessKey';
